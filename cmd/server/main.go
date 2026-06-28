284fb12299aefae9e404955ef3e8c739509305c0
izumiicxde <izumii.code@gmail.com>
2026-06-28 21:59:48 +0530

    fix: minor changes

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,25 @@
 package main
 
-import "concurrent-job-processing-system/internal/api"
+import (
+	"concurrent-job-processing-system/internal/api"
+	"concurrent-job-processing-system/internal/config"
+	"concurrent-job-processing-system/internal/logger"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	server := api.New()
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+	cfg := config.Load()
+
+	log := logger.New(cfg)
+	server := api.New(log)
 	server.Run()
+
+	if err := log.Close(); err != nil {
+		panic(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module concurrent-job-processing-system
 
 go 1.26
+
+require (
+	github.com/caarlos0/env/v11 v11.4.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
