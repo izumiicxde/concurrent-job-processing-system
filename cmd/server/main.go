061e5849217c98df62ada26ba91ec20bc479993c
izumiicxde <izumii.code@gmail.com>
2026-07-05 19:52:34 +0530

    feat: setup the system lifecycle

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,7 +3,13 @@ package main
 import (
 	"concurrent-job-processing-system/internal/api"
 	"concurrent-job-processing-system/internal/config"
+	"concurrent-job-processing-system/internal/executor"
 	"concurrent-job-processing-system/internal/logger"
+	"concurrent-job-processing-system/internal/queue"
+	"concurrent-job-processing-system/internal/store"
+	"concurrent-job-processing-system/internal/worker"
+	"context"
+	"os"
 
 	"github.com/joho/godotenv"
 )
@@ -16,7 +22,16 @@ func main() {
 	cfg := config.Load()
 
 	log := logger.New(cfg)
-	server := api.New(cfg, log)
-	server.Run()
+	memoryStore := store.New()
+	memoryQueue := queue.NewMemoryQueue(200)
+	jobExecutor := executor.New(log)
 
+	workersPool := worker.NewWorkerPool(20, memoryQueue, memoryStore, jobExecutor, log)
+	server := api.New(cfg, log, memoryQueue, memoryStore, jobExecutor)
+
+	workersPool.Start()
+	server.Run()
+	if err := workersPool.Shutdown(context.Background()); err != nil {
+		os.Exit(1)
+	}
 }
